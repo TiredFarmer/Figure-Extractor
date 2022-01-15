96c7ccbfae7c9a62aeb4c2583f024f5d6a55d54a
Yifan Hao <63517269+strvight@users.noreply.github.com>
2022-01-15 12:57:21 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+    #return "<p>Hello, World!</p>"
+
+#     @app.route('/hello/')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
