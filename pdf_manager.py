82e40a366243c0329cfab22bee85473a4ab6a348
Patrick <patrickhong21@hotmail.com>
2022-01-15 15:06:24 -0800

    added comments

--- pdf_manager.py
+++ pdf_manager.py
@@ -1,18 +1,20 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
+# from image_extractor import get_pdf_images
 
 # to be changed later
 PATH = ""
 
 def split_pdf_get_images(pdf_name: str, start: int, end: int) -> None:
-    pdf_in = PdfFileReader(PATH + pdf_name)
+    pdf_in = PdfFileReader(PATH + pdf_name + ".pdf")
     pdf_out = PdfFileWriter()
 
     for page in range(start, end + 1):
         pdf_out.addPage(pdf_in.getPage(page))
 
-
-    with open("out.pdf", 'wb') as output_pdf:
+    with open(pdf_name + "_split.pdf", 'wb') as output_pdf:
         pdf_out.write(output_pdf)
 
+    #get_pdf_images()
+
 if __name__ == "__main__":
-    split_pdf_get_images("in.pdf", 1, 900)
+    split_pdf_get_images("in_split", 1, 750)
