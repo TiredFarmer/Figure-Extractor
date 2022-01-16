20407bd4fc61fa15a15f807778f2b847bddf85c9
William <william@gameofapps.org>
2022-01-15 16:37:10 -0800

    image extractor, bounding box images

--- image_extractor.py
+++ image_extractor.py
@@ -4,23 +4,42 @@ import fitz
 import io
 from PIL import Image
 
+# TODO what if no images?
 def get_pdf_images(doc):
     images = []
+    rects = []
 
     for i in range(doc.pageCount):
-        for item in doc.getPageImageList(i):
+        page = doc[i]
+        page_rects = []
+        for item in page.get_images(full = True):
             pix = fitz.Pixmap(doc, item[0])  # pixmap from the image xref
             pix0 = fitz.Pixmap(fitz.csRGB, pix)  # force into RGB
-            data = pix0.getImageData()
+            data = pix0.tobytes()
             img = Image.open(io.BytesIO(data))
             images.append(img)
-    
+
+            rect = page.get_image_bbox(item)
+            page_rects.append(rect)
+            page.draw_rect(rect, color=[0,1,1,0], overlay=True,width=0.5,fill_opacity=1)
+
+        
+        rects.append(page_rects)
+        print(page_rects)
+        pix = page.getPixmap()
+        page_image = Image.open(io.BytesIO(pix.tobytes()))
+        page_image.show()
+        if i >= 4:
+            break
+
+    print(len(images))
+    print(len(rects))
+    print(rects)    
     images[0].save("raw_pdfs\extracted.pdf", save_all = True, append_images = images[1:])
 
   
-file = "raw_pdfs\Scott Freeman Kim Quillin Lizabeth Allison Michael Black Greg Podgorski Emily Taylor Jeff Carmichael Michael Harrington Joan C. Sharp - Biological Science, Third Canadian Edition, 3rd edition-Pearson  (1).pdf"
+file = "raw_pdfs\\bio_book.pdf"
 
 pdf_file = fitz.open(file)
 
 get_pdf_images(pdf_file)
-  
