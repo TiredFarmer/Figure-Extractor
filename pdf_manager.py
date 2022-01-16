d3ff7a125e1245b0f03df4eca764e0e90a24f8e4
Patrick <patrickhong21@hotmail.com>
2022-01-15 16:40:14 -0800

    added -1 for pages

--- pdf_manager.py
+++ pdf_manager.py
@@ -1,17 +1,21 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from image_extractor import get_pdf_images, opener
 
-# to be changed later
-PATH = ""
+PATH = "pdf/"
 
 def split_pdf_get_images(pdf_name: str, start: int, end: int) -> None:
+
     pdf_in = PdfFileReader(PATH + pdf_name + ".pdf")
     pdf_out = PdfFileWriter()
 
-    for page in range(start, end + 1):
+    if end == -1:
+        end = pdf_in.getNumPages()
+
+
+    for page in range(start - 1, end):
         pdf_out.addPage(pdf_in.getPage(page))
 
-    new_name = pdf_name + "_split.pdf"
+    new_name = PATH + pdf_name + "_split.pdf"
 
     with open(new_name, 'wb') as output_pdf:
         pdf_out.write(output_pdf)
@@ -19,4 +23,4 @@ def split_pdf_get_images(pdf_name: str, start: int, end: int) -> None:
     get_pdf_images(opener(new_name))
 
 if __name__ == "__main__":
-    split_pdf_get_images("in", 1, 15)
+    split_pdf_get_images("in", 1, -1)
