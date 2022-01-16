f2beaeda509ea1fa14ebc4f9aaead8bd62d1c9cd
strvight <yifanhao5@gmail.com>
2022-01-15 20:58:42 -0800

    added default page values.

--- app.py
+++ app.py
@@ -41,20 +41,23 @@ def upload_file():
 
         # Get page numbers
         form_data = request.form
+
         start_page = int(form_data['start_page'])
         end_page = int(form_data['end_page'])
+    
+      
 
         if start_page > end_page and end_page != -1:
             flash('End page must be greater than start page')
             return redirect(request.url)
 
-        print(start_page, end_page)
+        # print(start_page, end_page)
 
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             filename_nodot = filename.split(".pdf")[0]
-            print(filename)
+           # print(filename)
             final_name = split_pdf_get_images(filename_nodot, start_page, end_page) + ".pdf"
             return redirect('/downloadfile/'+ final_name)
             #return redirect('/downloadfile/extracted.pdf')
