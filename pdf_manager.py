7495dd56f1965b8c637fff34c6ea06fd58eaf9c8
Patrick <patrickhong21@hotmail.com>
2022-01-15 16:43:25 -0800

    returns final name

--- pdf_manager.py
+++ pdf_manager.py
@@ -1,9 +1,12 @@
+from asyncio import streams
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from image_extractor import get_pdf_images, opener
 
 PATH = "pdf/"
 
-def split_pdf_get_images(pdf_name: str, start: int, end: int) -> None:
+def split_pdf_get_images(pdf_name: str, start: int, end: int) -> str:
+
+    FINAL_NAME = "extracted"
 
     pdf_in = PdfFileReader(PATH + pdf_name + ".pdf")
     pdf_out = PdfFileWriter()
@@ -20,7 +23,9 @@ def split_pdf_get_images(pdf_name: str, start: int, end: int) -> None:
     with open(new_name, 'wb') as output_pdf:
         pdf_out.write(output_pdf)
 
-    get_pdf_images(opener(new_name))
+    get_pdf_images(opener(new_name), FINAL_NAME)
+
+    return FINAL_NAME
 
 if __name__ == "__main__":
-    split_pdf_get_images("in", 1, -1)
+    print(split_pdf_get_images("in", 1, -1))
